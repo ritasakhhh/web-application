b0d461c5794d91d7c4f1dc77e05c9fe9aaf972db
Rita <margarita20070318@gmail.com>
2026-09-15 11:35:40 +0300

    first_step: добавить CRUD и индивидуальную выборку

--- first_step.py
+++ first_step.py
@@ -73,3 +73,148 @@ def update_actor(
             return updated_actor
 
     raise ValueError("Actor not found")
+
+def create_instruction(
+    input_text: str,
+    actor_id: int,
+    description: str,
+    tags: str,
+    stage: str,
+) -> tuple:
+    if not has_id(actors, actor_id):
+        raise ValueError("Actor not found")
+
+    instruction = (
+        get_next_id(instructions),
+        get_current_time(),
+        input_text,
+        actor_id,
+        description,
+        tags,
+        stage,
+    )
+
+    instructions.append(instruction)
+    return instruction
+
+
+def get_instructions() -> list:
+    return instructions
+
+
+def delete_instruction(uid: int) -> bool:
+    for instruction in instructions:
+        if instruction[0] == uid:
+            instructions.remove(instruction)
+            return True
+
+    raise ValueError("Instruction not found")
+
+
+def update_instruction(
+    uid: int,
+    input_text: str,
+    actor_id: int,
+    description: str,
+    tags: str,
+    stage: str,
+) -> tuple:
+    if not has_id(actors, actor_id):
+        raise ValueError("Actor not found")
+
+    for index, instruction in enumerate(instructions):
+        if instruction[0] == uid:
+            updated_instruction = (
+                instruction[0],
+                instruction[1],
+                input_text,
+                actor_id,
+                description,
+                tags,
+                stage,
+            )
+
+            instructions[index] = updated_instruction
+            return updated_instruction
+
+    raise ValueError("Instruction not found")
+
+
+def create_response(
+    output: str,
+    stage: str,
+    failure: str,
+    instruction_id: int,
+) -> tuple:
+    if not has_id(instructions, instruction_id):
+        raise ValueError("Instruction not found")
+
+    response = (
+        get_next_id(responses),
+        get_current_time(),
+        output,
+        stage,
+        failure,
+        instruction_id,
+    )
+
+    responses.append(response)
+    return response
+
+
+def get_responses() -> list:
+    return responses
+
+
+def delete_response(uid: int) -> bool:
+    for response in responses:
+        if response[0] == uid:
+            responses.remove(response)
+            return True
+
+    raise ValueError("Response not found")
+
+
+def update_response(
+    uid: int,
+    output: str,
+    stage: str,
+    failure: str,
+    instruction_id: int,
+) -> tuple:
+    if not has_id(instructions, instruction_id):
+        raise ValueError("Instruction not found")
+
+    for index, response in enumerate(responses):
+        if response[0] == uid:
+            updated_response = (
+                response[0],
+                response[1],
+                output,
+                stage,
+                failure,
+                instruction_id,
+            )
+
+            responses[index] = updated_response
+            return updated_response
+
+    raise ValueError("Response not found")
+
+
+def get_recent_responses() -> list:
+    current_time = get_current_time()
+    result = []
+
+    for instruction in instructions:
+        if instruction[1] >= current_time - SIX_MINUTES:
+            for response in responses:
+                if instruction[0] == response[5]:
+                    result.append(
+                        (
+                            instruction[4],
+                            response[2],
+                        )
+                    )
+
+    return result
